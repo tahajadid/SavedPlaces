93d7e66e1bfd9f51803988b0d223c4731f8baa17
Taha Jadid <taha.jadid@inwi.ma>
2022-07-26 15:52:16 +0100

    Add Permission and data class

---
+++ app/src/main/java/tahadeta/example/savedplaces/FavouritePlace.kt
@@ -0,0 +1,7 @@
+package tahadeta.example.savedplaces
+
+data class FavouritePlace(
+    val lat: Double = 0.0,
+    val lng: Double = 0.0,
+    val title: String = ""
+)
--- app/src/main/java/tahadeta/example/savedplaces/MapsActivity.kt
+++ app/src/main/java/tahadeta/example/savedplaces/MapsActivity.kt
@@ -1,8 +1,17 @@
 package tahadeta.example.savedplaces
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -13,8 +22,17 @@ import tahadeta.example.savedplaces.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private var permissionLocation = false
+    private lateinit var mylocation: ImageView
+
+    var currentLocation: Location? = null
+    var fusedLocationProviderClient: FusedLocationProviderClient? = null
+
+    lateinit var actualGoogleMap: GoogleMap
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,27 +40,140 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
+
+        mylocation = findViewById(R.id.mylocation_iv)
+        mylocation.setOnClickListener {
+            fetchLocation()
+            // goToThePlace()
+        }
+
+        enableMyLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        actualGoogleMap = googleMap
+        /*
+        if (permissionLocation) map.isMyLocationEnabled = true
+        Log.d(
+            "MapsActivityLog",
+            "######## State of Localisation =:= " +
+                LocationHelper.isLocationEnabled(this.baseContext)
+        )
+
+         */
+
+        val latLng = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+        Log.d("MapsActivityLog", "** task value : ${latLng.longitude}")
+
+        val markerOptions = MarkerOptions().position(latLng).title("I Am Here!")
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+        googleMap.addMarker(markerOptions)
+
+        /*
+        val intent1 = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent1)
+         */
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        val task = fusedLocationProviderClient!!.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                Log.d("MapsActivityLog", "******* task value : ${location.longitude}")
+
+                val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?)
+                supportMapFragment!!.getMapAsync(this@MapsActivity)
+            }
+        }
     }
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Function to change place
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    fun goToThePlace(lat: Double, lng: Double, title: String) {
+        val newlatLng = LatLng(lat, lng)
+        val markerOptions = MarkerOptions().position(newlatLng).title(title)
+        actualGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(newlatLng))
+        actualGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(newlatLng, 15f))
+        actualGoogleMap.addMarker(markerOptions)
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+
+        // 1. Check if permissions are granted, if so, enable the my location layer
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("MapsActivityLog", "Inside on 1")
+            permissionLocation = true
+            return
+        }
+
+        // 2. If a permission rationale dialog should be shown
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+        ) {
+            Log.d("MapsActivityLog", "Inside on 2")
+            return
+        }
+
+        // 3. Otherwise, request permission
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+
+        Log.d("MapsActivityLog", "Inside on enableMyLocation()")
+    }
+
+    /**
+     * onRequestPermissionsResult
+     */
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d("MapsActivityLog", "Inside on permission result 111")
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            map.isMyLocationEnabled = true
+            Log.d("MapsActivityLog", "Inside on permission result 222")
+        }
     }
 }
